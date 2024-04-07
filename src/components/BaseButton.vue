67537f86a64c54c643ac04d67c57a597f9a99335
Zoltan Keresztes <zolikeresztes90@gmail.com>
2024-04-07 22:58:25 +0300

    Create and style base button elements

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+  type BtnType = 'primary' | 'default';
+  
+  interface BaseButtonProps {
+    type?: BtnType;
+    isDisabled?: boolean;
+  }
+
+  const props = defineProps<BaseButtonProps>()
+</script>
+<template>
+  <button :class="type" :disabled="isDisabled">
+    <slot>
+    </slot>
+  </button>
+</template>
+<style scoped lang="scss">
+  %button-shared {
+    border-radius: 10px;
+    width: 100%;
+    padding: 10px 0;
+  }
+
+  .primary {
+    @extend %button-shared;
+    background-color: $primary-color;
+    color: $secondary-text-color;
+  }
+
+  .default {
+    @extend %button-shared;
+    color: $tertiary-text-color;
+    border-color: $primary-color;
+    background-color: $secondary-color;
+  }
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import BaseButton from '../components/BaseButton.vue';
 </script>
 <template>
   <h1>Learn Easy Code</h1>
+  <BaseButton type="default">Edit</BaseButton>
+  <BaseButton type="primary">Submit</BaseButton>
 </template>
 <style lang="scss">
   h1 {
