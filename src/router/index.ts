bda9d2195b995af37c529d82a3fb8e349171cf4e
Zoltan Keresztes <zolikeresztes90@gmail.com>
2024-04-21 16:55:59 +0300

    check if user is set or redirect to login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,37 @@
+import { useAuthStore } from '@/stores/auth'
+import { storeToRefs } from 'pinia'
 import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/LoginView.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('@/views/RegisterView.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: () => import('@/views/DashboardView.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('@/views/LoginView.vue')
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: () => import('@/views/RegisterView.vue')
-    },
-    {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: () => import('@/views/DashboardView.vue')
-    }
-  ]
+  routes
+})
+
+router.beforeEach(async (to) => {
+  const authStore = useAuthStore()
+  const { user } = storeToRefs(authStore)
+
+  if (!user.value && to.name !== 'login') {
+    return { name: 'login' }
+  }
 })
 
 export default router
